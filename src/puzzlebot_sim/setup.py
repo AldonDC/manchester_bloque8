eb9576cabe97610551e7365f129d8fd9bb01fc86
AldonDC <a00838034@tec.mx>
2026-03-27 11:42:32 -0600

    Copied Week 1 Challenge base from professor_repo to src/puzzlebot_sim

--- src/puzzlebot_sim/setup.py
+++ src/puzzlebot_sim/setup.py
@@ -1,6 +1,8 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
-package_name = 'src/puzzlebot_sim'
+package_name = 'puzzlebot_sim'
 
 setup(
     name=package_name,
@@ -10,21 +12,22 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.[yma]*'))),
+        (os.path.join('share', package_name, 'rviz'), glob(os.path.join('rviz', '*.rviz'))),
+        (os.path.join('share', package_name, 'meshes'), glob(os.path.join('meshes', '*.stl'))),
+        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.urdf'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='alfonso',
-    maintainer_email='a00838034@tec.mx',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    maintainer='Mario Martinez',
+    maintainer_email='mario.mtz@manchester-robotics.com',
+    description='Puzzlebot Kinematic Sim',
+    license='Apache-2.0',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'puzzlebot_sim = src/puzzlebot_sim.puzzlebot_sim:main'
+            'joint_state_publisher = puzzlebot_sim.joint_state_publisher:main'
         ],
     },
 )
--- src/puzzlebot_sim/src/puzzlebot_sim/puzzlebot_sim.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print('Hi from src/puzzlebot_sim.')
-
-
-if __name__ == '__main__':
-    main()
